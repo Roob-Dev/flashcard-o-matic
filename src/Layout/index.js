9b28f877e2134a83e3e78ce3c206364785be4d99
rubi-gallegos <rubi.g.llam@gmail.com>
2021-08-02 00:46:03 -0500

    completed assignment

---
+++ src/Layout/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+// include all imports
+import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
+import NotFound from "./NotFound";
+import Home from "../Home";
+import CreateDeck from "../decks/CreateDeck"
+import Deck from "../decks/Deck";
+import Study from "../Study";
+import EditDeck from "../decks/EditDeck";
+import AddCard from "../cards/AddCard";
+import EditCard from "../cards/EditCard";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        {/* TODO: Implement the screen starting here */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/decks/new">
+            <CreateDeck />
+          </Route>
+          <Route exact path="/decks/:deckId">
+            <Deck />
+          </Route>
+          <Route path="/decks/:deckId/study">
+            <Study />
+          </Route>
+          <Route path="/decks/:deckId/edit">
+            <EditDeck />
+          </Route>
+          <Route path="/decks/:deckId/cards/new">
+            <AddCard />
+          </Route>
+          <Route path="/decks/:deckId/cards/:cardId/edit">
+            <EditCard />
+          </Route>
+          <NotFound />
+        </Switch>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
